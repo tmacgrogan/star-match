5fc394bf633a12c316180d20d941221815c35c12
Tyler MacGrogan <tylertylermac@gmail.com>
2020-02-17 23:53:29 -0500

    sync

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Game from './Game';
+
+// STAR MATCH - V9
+
+const StarMatch = () => {
+  const [gameId, setGameId] = useState(1);
+  return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
+};
+
+export function App() {
+  return <StarMatch />;
+}
